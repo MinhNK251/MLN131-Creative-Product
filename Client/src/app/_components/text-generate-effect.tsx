dd347cc51b4bd9126d0689fe22145bb16c464bc1
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-09-16 12:39:01 +0700

    remove projects

--- Client/src/app/_components/text-generate-effect.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
-
-const words = ` 
-`;
-
-export function TextGenerateEffectDemo() {
-  return <TextGenerateEffect words={words} />;
-}
